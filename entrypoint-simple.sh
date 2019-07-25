c14a9fd5dbd5e5ba71a2d58b6e0b9e47e5366bb8
Chuan Miao <chuan137@gmail.com>
2019-07-25 16:17:14 +0200

    check changes in netapp-filers.yaml periodically

--- entrypoint-simple.sh
+++ entrypoint-simple.sh
@@ -3,22 +3,23 @@ set -e
 
 NETAPP_HOME=${NETAPP_HOME:-/opt/netapp-harvest}
 NETAPP_FILERS_YAML=${NETAPP_HOME}/config/netapp-filers.yaml
-NETAPP_HARVEST_CONF=${NETAPP_HOME}/config/netapp-harvest.conf
+NETAPP_HARVEST_CONF_TMPL=${NETAPP_HOME}/config/netapp-harvest.conf
+NETAPP_HARVEST_CONF=${NETAPP_HOME}/netapp-harvest.conf
 NETAPP_USERNAME=${NETAPP_USERNAME:-admin}
 NETAPP_PASSWORD=${NETAPP_PASSWORD:-netapp123}
 
 #===============================================================================
 
 function read_name() {
-  local name
-  name=`echo "$1.name" | xargs yq r $NETAPP_FILERS_YAML`
-  echo "$name"
+    local name
+    name=`echo "$1.name" | xargs yq r $NETAPP_FILERS_YAML`
+    echo "$name"
 }
 
 function read_host() {
-  local host
-  host=`echo "$1.host" | xargs yq r $NETAPP_FILERS_YAML`
-  echo "$host"
+    local host
+    host=`echo "$1.host" | xargs yq r $NETAPP_FILERS_YAML`
+    echo "$host"
 }
 
 function set_filer_group() {
@@ -27,46 +28,59 @@ function set_filer_group() {
     fi
 }
 function write_config() {
-  if [ "x$1" == "x" ]; then
-    return 1
-  fi
-  local name
-  local host
-  name=$(read_name $1)
-  host=$(read_host $1)
-  set_filer_group $name
-  echo "[$name]"
-  echo "hostname = $host"
-  echo "username = $username"
-  echo "password = $password"
-  echo "group = $FILER_GROUP"
+    if [ "x$1" == "x" ]; then
+        return 1
+    fi
+    local name
+    local host
+    name=$(read_name $1)
+    host=$(read_host $1)
+    set_filer_group $name
+    echo "[$name]"
+    echo "hostname = $host"
+    echo "username = $NETAPP_USERNAME"
+    echo "password = $NETAPP_PASSWORD"
+    echo "group = $FILER_GROUP"
 }
 
 
 if [ ! -f "$NETAPP_FILERS_YAML" ]; then 
-  echo "file $NETAPP_FILERS_YAML does not exist"
-  exit 1
+    echo "file $NETAPP_FILERS_YAML does not exist"
+    exit 1
 fi
 
-i=0
-while [ $(read_name $i) != "null" ]; do
-  write_config $i >> $NETAPP_HARVEST_CONF
-  echo >> $NETAPP_HARVEST_CONF
-  i=$((i+1))
-done
-
-# netapp manager start 
-${NETAPP_HOME}/netapp-manager -start \
-    -confdir ${NETAPP_HOME}/config \
-    -logdir ${NETAPP_HOME}/log
-    #-group ${FILER_GROUP} \
+cp $NETAPP_HARVEST_CONF_TMPL $NETAPP_HARVEST_CONF
+checksum=
 
 # check status every 10 seconds
+# check netapp-filers.yaml
 while true; do
-    if ls ${NETAPP_HOME}/log/*.log 1> /dev/null 2>&1; then
-        tail -f ${NETAPP_HOME}/log/*.log
-    else
-        echo "sleep 10 seconds"
-        sleep 10
+    newchecksum=$(cat $NETAPP_FILERS_YAML | md5sum | cut -d ' ' -f 1)
+    if [ "$checksum" != "$newchecksum" ]; then
+        checksum=$newchecksum
+        ${NETAPP_HOME}/netapp-manager -stop \
+            -confdir ${NETAPP_HOME} \
+            -logdir ${NETAPP_HOME}/log
+
+        i=0
+        cp $NETAPP_HARVEST_CONF_TMPL $NETAPP_HARVEST_CONF
+        while [ $(read_name $i) != "null" ]; do
+            write_config $i >> $NETAPP_HARVEST_CONF
+            echo >> $NETAPP_HARVEST_CONF
+            i=$((i+1))
+        done
+
+        ${NETAPP_HOME}/netapp-manager -start \
+            -confdir ${NETAPP_HOME} \
+            -logdir ${NETAPP_HOME}/log
     fi
+
+
+    # if ls ${NETAPP_HOME}/log/*.log 1> /dev/null 2>&1; then
+    #     tail -f ${NETAPP_HOME}/log/*.log
+    # fi
+    ${NETAPP_HOME}/netapp-manager -status \
+        -confdir ${NETAPP_HOME}
+    echo "sleep 60 seconds"
+    sleep 60
 done

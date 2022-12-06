76adf0c6f0f763bbbf98c69ce57f0f37b119a5db
chuan137 <chuan137@gmail.com>
2022-12-06 14:38:27 +0100

    check filers yaml file with backoff timeout before start workers

--- entrypoint-simple.sh
+++ entrypoint-simple.sh
@@ -35,6 +35,7 @@ function set_filer_group() {
         FILER_GROUP="unknown"
     fi
 }
+
 function write_config() {
     if [ "x$1" == "x" ]; then
         return 1
@@ -51,44 +52,68 @@ function write_config() {
     echo "group = $FILER_GROUP"
 }
 
+function check_filer_with_backoff {
+    # check filer file with backoff timeout 1, 2, 4, 8, ..., 256, 300, 300, ...
+    local timeout_max=300
+    local timeout=${TIMEOUT-1}
+    local attempt=0
 
-if [ ! -f "$NETAPP_FILERS_YAML" ]; then 
-    echo "file $NETAPP_FILERS_YAML does not exist"
-    exit 1
-fi
+    while true;
+    do
+        if [ ! -f "$NETAPP_FILERS_YAML" ]; then 
+            echo "File $NETAPP_FILERS_YAML not found. Retrying in $timeout.."
+        else
+            break
+        fi
 
-cp $NETAPP_HARVEST_CONF_TMPL $NETAPP_HARVEST_CONF
+        sleep $timeout
+        attempt=$(( attempt + 1 ))
+        timeout=$(( timeout * 2 ))
+        if [ "$timeout" -gt "$timeout_max" ]; then
+            timeout=$timeout_max
+        fi
+    done
+}
+
+# check netapp-filers.yaml every 300 seconds
 checksum=
 
-# check status every 60 seconds
-# check netapp-filers.yaml
 while true; do
-    newchecksum=$(cat $NETAPP_FILERS_YAML | md5sum | cut -d ' ' -f 1)
-    if [ "$checksum" != "$newchecksum" ]; then
-        checksum=$newchecksum
-        ${NETAPP_HOME}/netapp-manager -stop \
-            -confdir ${NETAPP_HOME} \
-            -logdir ${NETAPP_HOME}/log
+    check_filer_with_backoff
 
+    restart=0
+    newchecksum=$(cat $NETAPP_FILERS_YAML | md5sum | cut -d ' ' -f 1)
+
+    if [ "$checksum" != "$newchecksum" ]; then
+        # restart if checksum is different
+        restart=1
+        checksum=$newchecksum
+
+        echo "(new) file $NETAPP_FILERS_YAML found"
+        cat $NETAPP_FILERS_YAML
+
+        # read $NETAPP_FILERS_YAML and re-generate $NETAPP_HARVEST_CONF
         i=0
         cp $NETAPP_HARVEST_CONF_TMPL $NETAPP_HARVEST_CONF
         while [ $(read_name $i) != "null" ]; do
             write_config $i >> $NETAPP_HARVEST_CONF
             echo >> $NETAPP_HARVEST_CONF
-            i=$((i+1))
+            i=$(( i + 1 ))
         done
-
-        ${NETAPP_HOME}/netapp-manager -start \
-            -confdir ${NETAPP_HOME} \
-            -logdir ${NETAPP_HOME}/log
+    else
+        # check netapp manager status: restart when no worker is running
+        running_workers=$(${NETAPP_HOME}/netapp-manager -status -confdir ${NETAPP_HOME} | grep '^\[RUNNING\]' | wc -l)
+        restart=$running_workers
     fi
 
+    if [ "$restart" -gt "0" ]; then
+        ${NETAPP_HOME}/netapp-manager -stop -confdir ${NETAPP_HOME} -logdir ${NETAPP_HOME}/log
+        ${NETAPP_HOME}/netapp-manager -start -confdir ${NETAPP_HOME} -logdir ${NETAPP_HOME}/log
+    fi
 
     # if ls ${NETAPP_HOME}/log/*.log 1> /dev/null 2>&1; then
     #     tail -f ${NETAPP_HOME}/log/*.log
     # fi
-    # ${NETAPP_HOME}/netapp-manager -status \
-    #     -confdir ${NETAPP_HOME}
-    # echo "sleep 60 seconds"
-    sleep 60
+    # echo "sleep 300 seconds"
+    sleep 300
 done
